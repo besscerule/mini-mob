3b89eeca45fa05c0d411d138c0c7e503b2efb617
Mark <matovey@gmail.com>
2022-06-14 17:53:52 +0100

    wip

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,28 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import {waitForAsync, TestBed} from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+describe(`AppComponent'`, (() => {
+  const fixture = TestBed.createComponent(AppComponent)
+  const app = fixture.debugElement.componentInstance
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+  test(`it creates App component`, () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mini-mob'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+  test(`app should have as title 'mini-mob'`, () => {
     expect(app.title).toEqual('mini-mob');
   });
-});
+}));
