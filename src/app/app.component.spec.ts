6b506aec5fb13c474a9d123b3bcb97489cf00cd3
Mark <matovey@gmail.com>
2022-06-14 20:29:46 +0100

    chore: added jest unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,41 @@
-import {waitForAsync, TestBed} from '@angular/core/testing';
+import {
+  waitForAsync,
+  ComponentFixture,
+  TestBed
+} from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+}
+  from '@angular/platform-browser-dynamic/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe(`AppComponent'`, (() => {
-  const fixture = TestBed.createComponent(AppComponent)
-  const app = fixture.debugElement.componentInstance
+TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
 
-  test(`it creates App component`, () => {
-    expect(app).toBeTruthy();
+describe(`AppComponent'`, (() => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
-  test(`app should have as title 'mini-mob'`, () => {
-    expect(app.title).toEqual('mini-mob');
+  it(`creates App component`, () => {
+    expect(component).toBeDefined();
+  });
+
+  it(`should have title 'mini-mob'`, () => {
+    expect(component.title).toEqual('mini-mob');
   });
 }));
